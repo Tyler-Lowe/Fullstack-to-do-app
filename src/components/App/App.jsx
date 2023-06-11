4022c102f14c6e0846b419703794c01fbade72a6
Tyler Lowe <tlowe3420@gmail.com>
2023-06-10 21:37:20 -0500

    updated app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 import TaskCreator from '../TaskCreator';
 import NewTaskContainer from './NewTaskContainer';
-
+// create a state that will refresh on every form submission/task completed/task deleted
+// Need to add each todo item to the page not just the newest one
 
 function App () {
   
